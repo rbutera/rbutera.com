55ef0a261d213300aab73f35df64b72b6a3afcf7
Rai Butera <rai@rbutera.com>
2020-01-08 14:00:03 +0000

    add gatsby-transformer-json and gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,8 +29,16 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-postcss`,
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data`,
+      },
+    },
   ],
 }
