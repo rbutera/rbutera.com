638d3c7c5294c1cd7130d15fe73df1387d9d31dc
Rai Butera <rai@rbutera.com>
2020-01-08 14:21:55 +0000

    add aktiv grotesk font

--- src/global-style.tsx
+++ src/global-style.tsx
@@ -1,13 +1,14 @@
 import { createGlobalStyle } from "styled-components"
 import tw from "tailwind.macro"
 import "./tailwind.css"
+import "./fonts.css"
 
 export default createGlobalStyle`
   /* apply a natural box layout model to all elements, but allowing components to change */
   
   body {
     ${tw`m-0 p-0 font-sans bg-white text-gray-900`};
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    font-family: 'aktiv-grotesk', -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
     -webkit-text-size-adjust: 100%;
     width: 100vw;
     max-width: 100vw;
