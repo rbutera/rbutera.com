214a7669c10a0dd02a292e1135e2371ed2ebd361
Rai Butera <rai@rbutera.com>
2020-01-16 14:14:46 +0000

    reveal animations only play once

---
+++ src/components/VSensor.tsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import VisibilitySensor from "react-visibility-sensor"
+
+type VSensorProps = {
+  children: Function
+  once: boolean
+}
+
+type VSensorState = {
+  active: boolean
+}
+
+class VSensor extends Component<VSensorProps, VSensorState> {
+  constructor(props: VSensorProps) {
+    super(props)
+    this.state = {
+      active: true,
+    }
+  }
+
+  render() {
+    const { active } = this.state
+    const { once, children, ...theRest } = this.props
+    return (
+      <VisibilitySensor
+        active={active}
+        onChange={isVisible =>
+          once &&
+          isVisible &&
+          this.setState({ active: false }, () =>
+            console.log("turned the thing off!")
+          )
+        }
+        {...theRest}
+      >
+        {({ isVisible }) => children({ isVisible })}
+      </VisibilitySensor>
+    )
+  }
+}
+
+export default VSensor
