e2880913510eeb1e986aa4fec4e6f59d76dd90f4
Rai Butera <rai@rbutera.com>
2020-01-21 13:39:30 +0000

    add html2pdf.js

---
+++ html2pdf.js
@@ -0,0 +1,20 @@
+// html2pdf.js
+var page = new WebPage()
+var system = require("system")
+// change the paper size to letter, add some borders
+// add a footer callback showing page numbers
+page.paperSize = {
+  format: "Letter",
+  orientation: "portrait",
+  margin: { left: "2.5cm", right: "2.5cm", top: "1cm", bottom: "1cm" },
+  footer: {
+    height: "0.9cm",
+  },
+}
+page.zoomFactor = 1.5
+// assume the file is local, so we don't handle status errors
+page.open(system.args[1], function(status) {
+  // export to target (can be PNG, JPG or PDF!)
+  page.render(system.args[2])
+  phantom.exit()
+})
